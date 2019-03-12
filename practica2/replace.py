a5fd8daec1ce1f6e8700f415e87dca6a53ee0bf3
jaume <jaumecolomhernandez@gmail.com>
2019-03-12 10:39:27 +0100

    Afegit script per a cambiar lletres

---
+++ practica2/replace.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#SCRIPT TO REPLACE STRING IN ALL THE FILES
+#To correct the .lab in the database
+
+import fileinput
+import os
+import sys
+import glob
+
+for root, dirs, files in os.walk("./audios"):
+    for file in files:
+        if file.endswith(".lab"):
+            str_file = os.path.join(root, file)
+            with open(str_file) as f:
+                newText=f.read().replace('s', 'S')
+            
+            with open(str_file, "w") as f:
+                f.write(newText)
+        if file.endswith(".lab"):
+            with open(str_file) as f:
+                newText=f.read().replace('v', 'V')
+            
+            with open(str_file, "w") as f:
+                f.write(newText)
